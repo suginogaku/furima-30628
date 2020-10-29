b60347dd36e9bcdce7b733a0f4c4de1879806f2c
suginogaku <gakusugino@icloud.com>
2020-10-30 00:55:23 +0900

    新規登録、ログイン、ログアウト実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  
   validates :nickname, presence: true
   validates :email, uniqueness: true
-  validates :encrypted_password, format: { with: /\A[a-zA-Z0-9]+\z/ }
-  validates :first_name, presence: true, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ }
-  validates :last_name, presence: true, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'は英字と数字の両方を含めて設定してください' } 
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'は全角文字を使用してください' } do
+  validates :first_name
+  validates :last_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カナを使用してください' } do
+  validates :first_name_kana
+  validates :last_name_kana
+  end
+
   validates :birthday, presence: true
 end
+  
