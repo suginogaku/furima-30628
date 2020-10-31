d2ccb1c0e5107e233f6fbf6cee073919508f1421
suginogaku <gakusugino@icloud.com>
2020-10-31 18:14:04 +0900

    ActiveStorageの導入

--- Gemfile
+++ Gemfile
@@ -64,3 +64,5 @@ end
 gem 'devise'
 gem 'rails-i18n'
 gem 'pry-rails'
+gem 'mini_magick'
+gem 'image_processing', '~> 1.2'
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,11 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+
+  def item_params
+    # permitの記述にカラムを追加するかどうかわからない
+    params.require(:item).permit(:image).merge(user_id: current_user.id)
+  end
 end
