acae5e9416e607e45267a205d93140565655234e
suginogaku <gakusugino@icloud.com>
2020-10-31 12:52:35 +0900

    モデルの単体テストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,21 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   validates :nickname, presence: true
   validates :email, uniqueness: true
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'は英字と数字の両方を含めて設定してください' } 
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'は英字と数字の両方を含めて設定してください' }
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'は全角文字を使用してください' } do
-  validates :first_name
-  validates :last_name
+    validates :first_name
+    validates :last_name
   end
 
   with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カナを使用してください' } do
-  validates :first_name_kana
-  validates :last_name_kana
+    validates :first_name_kana
+    validates :last_name_kana
   end
 
   validates :birthday, presence: true
 end
-  
---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    nickname { Faker::Name.name }
+    email { Faker::Internet.free_email }
+    password = Faker::Internet.password(min_length: 6)
+    password { password }
+    password_confirmation { password }
+    first_name { '鈴木' }
+    last_name { '一郎' }
+    first_name_kana { 'スズキ' }
+    last_name_kana { 'イチロウ' }
+    birthday { '1973-10-22' }
+  end
+end
