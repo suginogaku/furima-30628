06bbe9d172a03691d3cd6804de3fc0280be146f3
suginogaku <gakusugino@icloud.com>
2020-11-04 17:10:09 +0900

    商品詳細表示機能の実装

--- db/migrate/20201031080544_create_items.rb
+++ db/migrate/20201031080544_create_items.rb
@@ -3,11 +3,11 @@ class CreateItems < ActiveRecord::Migration[6.0]
     create_table :items do |t|
       t.string :item_name, null: false
       t.text :item_explain, null: false
-      t.integer :item_category_id, null: false
-      t.integer :item_status_id, null: false
-      t.integer :item_burden_id, null: false
-      t.integer :item_prefecture_id, null: false
-      t.integer :item_day_id, null: false
+      t.integer :category_id, null: false
+      t.integer :status_id, null: false
+      t.integer :burden_id, null: false
+      t.integer :prefecture_id, null: false
+      t.integer :day_id, null: false
       t.integer :item_price, null: false
       t.references :user, foreign_key: true
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -36,11 +36,11 @@ ActiveRecord::Schema.define(version: 2020_10_31_083033) do
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "item_name", null: false
     t.text "item_explain", null: false
-    t.integer "item_category_id", null: false
-    t.integer "item_status_id", null: false
-    t.integer "item_burden_id", null: false
-    t.integer "item_prefecture_id", null: false
-    t.integer "item_day_id", null: false
+    t.integer "category_id", null: false
+    t.integer "status_id", null: false
+    t.integer "burden_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "day_id", null: false
     t.integer "item_price", null: false
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
